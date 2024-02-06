7471dfeeee825dbb4c3d2f6d950a82ca74343859
Tobbelure <sorensentobias06@gmail.com>
2024-02-06 12:28:06 +0100

    Add question variable and server port

--- public/script.js
+++ public/script.js
@@ -12,20 +12,39 @@ const db = new sqlite3.Database("ValgDatabase.db");
 
 // Oppretter tabell for stemmer hvis tabellen ikke allerede eksisterer
 db.run(
-  "CREATE TABLE IF NOT EXISTS stemmer (id INTEGER PRIMARY KEY AUTOINCREMENT, kandidat TEXT)"
+  "CREATE TABLE IF NOT EXISTS stemmer (id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT)"
 );
 
 // virker med html javascript og css
 app.use(express.static("public"));
 
-add.post("/vote/:candidate", (req, res) => {
-  const candidate = req.params.candidate;
+add.post("/vote/:question", (req, res) => {
+  const question = req.params.question;
 
   // legger til stemmer i databasen
-  db.run("INSERT INTO stemmer (kandidat) VALUES (?)", [kandidat], (err) => {
+  db.run("INSERT INTO stemmer (question) VALUES (?)", [question], (err) => {
     if (err) {
-      return res.status(500).send("Feil skjedde med stemmen");
+      return res.status(500).send("Error med stemmen");
     }
     res.status(200).send("Stemme vellykket");
   });
+});
+
+// Får stemmene
+app.get("/votes", (req, res) => {
+  // henter stemme data fra datbasen
+  db.all(
+    "SELECT question, COUNT(*) as count FROM votes GROUP BY question",
+    (err, rows) => {
+      if (err) {
+        return res.status(500).send("Error for å få tak i stemmer");
+      }
+      res.json(rows);
+    }
+  );
+});
+
+// Starter localhost
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}`);
 });
